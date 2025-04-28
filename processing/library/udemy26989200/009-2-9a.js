9316ef60046f0f07d724f8f501b0ab847c3e2ea7
Mic <creadev.ninja@gmail.com>
2025-04-28 21:46:31 +0200

    animation item 9b, book 3

---
+++ processing/library/udemy26989200/009-2-9a.js
@@ -0,0 +1,11 @@
+function setup(){
+  createCanvas(400,400);
+}
+function draw(){ 
+  background(220);
+  rect(100,100,200,200,20,20,20,20); // x,y,w,h, radius A, radiub B, radius C, radius D
+  
+}
+/*
+  https://p5js.org/reference/p5/rect/
+*/
---
+++ processing/library/udemy26989200/009-2-9b.js
@@ -0,0 +1,11 @@
+function setup(){
+  createCanvas(400,400);
+}
+function draw(){ 
+  background(220);
+  arc(200,200,100,150,0,270,CHORD);
+  
+}
+/*
+  https://p5js.org/reference/p5/rect/
+*/
