06a476fb9fe32c176dba302f6fa0becc52877464
Mic <creadev.ninja@gmail.com>
2025-05-14 11:05:47 +0200

    animation 17  item 27, book 3

---
+++ processing/anim-practice-017.md
@@ -0,0 +1,12 @@
+# Anim-practice-017
+
+Library 
+- EAN : udemy26989200
+- Title : Introduction in creative coding with p5js
+- Author : Paper Crane Lab - Creative Technologist
+- exemple : 5.24 to 5.27
+- Certificate : yes
+
+Create a Generative portrait (Pointillsm)
+
+[Pattern](../processing/library/udemy26989200/017.html)
--- processing/library/udemy26989200/017.js
+++ processing/library/udemy26989200/017.js
@@ -2,15 +2,25 @@
 // 25.Concept : using image
 
 let catImage;
+let color;
 function preload() {
-  catImage = loadImage("https://placecats.com/400/400");
+  //catImage = loadImage("https://placecats.com/400/400");
+  catImage = loadImage("./images/catimage.jpg",catImage ,handleError);
+}
+function handleError(event) {
+  console.error('Oops ', event);
 }
 
 function setup() {
   createCanvas(400, 400);
+  background(220);
 }
 
 function draw() {
-  background(220);
-  image(catImage,0,0);
+  //background(220);
+  //image(catImage,0,0);
+  color = catImage.get(mouseX,mouseY);
+  fill(color);
+  noStroke();
+  ellipse(mouseX,mouseY,10,10);
 }
