3037b0781a4dd75b74f360978251270bbfe87d1d
Mic <creadev.ninja@gmail.com>
2025-05-22 21:33:05 +0200

    animation 22  item 3, book 4

--- processing/library/udemy26431596/script.js
+++ processing/library/udemy26431596/script.js
@@ -8,23 +8,87 @@ const playerImage = new Image();
 playerImage.src = 'shadow_dog.png';
 const spriteWidth = 575; 
 const spriteHeight = 523; 
-let frameX = 0; // on va pouvoir 
-let frameY = 5; // animer la bête <---------------------------
+let playerState = 'run';
+//let frameX = 0; // on va pouvoir 
+//let frameY = 4; // animer la bête <---------------------------
 let gameFrame = 0;
-let staggerFrames = 5; // cadre décaléé
+const staggerFrames = 5; // cadre décaléé
+const spriteAnimations = [];
+const animationStates = [
+    {
+        name: 'idle',
+        frames: 7,
+    },
+    {
+        name: 'jump',
+        frames: 7,
+    },
+    {
+        name: 'fall',
+        frames: 9,
+    },
+    {
+        name: 'run',
+        frames: 9,
+    },
+    {
+        name: 'dizzy',
+        frames: 11,
+    },
+    {
+        name: 'sit',
+        frames: 5,
+    },
+    {
+        name: 'roll',
+        frames: 7,
+    },
+    {
+        name: 'bite',
+        frames: 7,
+    },
+    {
+        name: 'ko',
+        frames: 12,
+    },
+    {
+        name: 'getHit',
+        frames: 4,
+    }
+];
+animationStates.forEach((state, index)=>{
+    let frames = {
+        loc: [],
+    }
+    for(let j=0; j<state.frames; j++){
+        let positionX = j * spriteWidth;
+        let positionY = index * spriteHeight;
+        frames.loc.push({x: positionX, y: positionY});
+    }
+    spriteAnimations[state.name] = frames;
+});
+console.log(spriteAnimations);
 
 function animate() {
     ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
-    ctx.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeight, spriteWidth,spriteHeight,0,0,spriteWidth,spriteHeight); // dog
-    if(gameFrame % staggerFrames == 0) { 
-        if(frameX < 4) { // <---------------------------------
-            frameX++; 
-        }else{
-            frameX = 0; // blanking and fast
-        }  
-    }
- 
+    let position = Math.floor(gameFrame/staggerFrames) % spriteAnimations[playerState].loc.length; 
+    /*  6 frames (animations)
+        gameFrame/staggerFrames         position
+        0/5 = 0     Math.floor(0) = 0   0%6 = 0             0%6 = 0  <--
+        1/5 = 0.2   Math.floor(0.2)=0   0%6 = 0             1%6 = 1
+        2/5 = 0.4   Math.floor(0.4)=0   0%6 = 0             2%6 = 2
+        3/5 = 0.6   Math.floor(0.6)=0   0%6 = 0             3%6 = 3
+        4/5 = 0.8   Math.floor(0.8)=0   0%6 = 0             4%6 = 4
+        5/5 = 1     Math.floor(1) = 1   1%6 = 1             5%6 = 5
+                                                            6%6 = 0  <--
+                                                            7%6 = 1
+                                                            ...
+    */
+    let frameX = spriteWidth * position;
+    let frameY = spriteAnimations[playerState].loc[position].y;
 
+    ctx.drawImage(playerImage, frameX, frameY, spriteWidth, spriteHeight,0,0,spriteWidth, spriteHeight); // dog
+    
     gameFrame++;
     requestAnimationFrame(animate);
 }
